a9170355fd1477863394041594817a514103a8e0
Arthur Philadelpho <arthur22rj@gmail.com>
2018-04-23 11:46:16 -0300

    Complex Problems

---
+++ tsp_greed.rb
@@ -0,0 +1,13 @@
+def nearest_possible_neighbour(graph_of_cities, current_city)
+	while current_city != true
+		neighbor_cities = current_city.neighbors
+		next_city = neighbor_cities[0]
+		for current_neighbor in neighbor_cities
+			if current_neighbor.distance < next_city.distance
+				next_city = current_neighbor
+			end
+		end
+		current_city.visited = true
+		current_city = next_city
+	end
+end
